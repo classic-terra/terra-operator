15d9b9c5ddd6f4541bacf81832c0417dd45c32eb
Tobias Andersen <toban@dfds.com>
2022-05-29 19:05:53 +0200

    Removed ephemeralstorage resourcer req

--- pkg/controller/terradnode/terradnode_controller.go
+++ pkg/controller/terradnode/terradnode_controller.go
@@ -189,11 +189,10 @@ func newPodForCR(cr *terrav1alpha1.TerradNode) *corev1.Pod {
 		},
 	}
 
-	// 4 CPUs, 32GB memory & 2TB of storage as minimum requirement @ https://docs.terra.money/docs/full-node/run-a-full-terra-node/system-config.html
+	// 4 CPUs & 32GB memory as minimum requirement @ https://docs.terra.money/docs/full-node/run-a-full-terra-node/system-config.html
 	minimumRequestLimits := corev1.ResourceList{
-		corev1.ResourceCPU:              resource.MustParse("4000m"),
-		corev1.ResourceMemory:           resource.MustParse("32Gi"),
-		corev1.ResourceEphemeralStorage: resource.MustParse("2Ti"),
+		corev1.ResourceCPU:    resource.MustParse("4000m"),
+		corev1.ResourceMemory: resource.MustParse("32Gi"),
 	}
 
 	return &corev1.Pod{
